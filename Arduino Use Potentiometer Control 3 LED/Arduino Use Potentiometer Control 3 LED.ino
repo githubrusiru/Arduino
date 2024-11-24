6f14d83f5d0045f07abb0294ca89925dc97c3718
githubrusiru <141903467+githubrusiru@users.noreply.github.com>
2024-11-24 00:22:26 -0800

    Add files via upload

---
+++ Arduino Use Potentiometer Control 3 LED/Arduino Use Potentiometer Control 3 LED.ino
@@ -0,0 +1,33 @@
+int i = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  i = analogRead(A0);
+  if (i < 400)
+  {
+    digitalWrite(13, 1);
+    digitalWrite(12, 0);
+    digitalWrite(11, 0);
+  }
+  else if(i < 900)
+  {
+    digitalWrite(13, 0);
+    digitalWrite(12, 1);
+    digitalWrite(11, 0);
+  }
+  else
+  {
+    digitalWrite(13, 0);
+    digitalWrite(12, 0);
+    digitalWrite(11, 1);
+  }
+
+}
